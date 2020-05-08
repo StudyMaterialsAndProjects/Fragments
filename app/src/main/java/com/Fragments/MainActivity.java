be8a46684ffc60f4541cb4929d978c0146877b7a
vinaytshetty <vinaytshetty@gmail.com>
2020-05-08 21:05:13 +0530

    Adding a Fragment Using a java Code

--- app/src/main/java/com/Fragments/MainActivity.java
+++ app/src/main/java/com/Fragments/MainActivity.java
@@ -1,8 +1,12 @@
 package com.Fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.Fragments.Fragments.SampleFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addFragment();
+    }
+    private void addFragment(){
+        FragmentManager loc_fragmentManager=getSupportFragmentManager();
+        FragmentTransaction loc_fragmentTransaction=loc_fragmentManager.beginTransaction();
+        SampleFragment sampleFragment=new SampleFragment();
+        loc_fragmentTransaction.add(R.id.fragment_Container,sampleFragment);
+        loc_fragmentTransaction.commit();
     }
 }
